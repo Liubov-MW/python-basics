6e83cee84af53cf064de06a8c73ef211ec4a9d61
Liubov-MW <Lybawa92@gmail.com>
2026-09-11 00:11:29 +0200

    Create exercises.py

---
+++ week03/exercises.py
@@ -0,0 +1,46 @@
+# WEEK 3 – LOOPS
+
+# Aufgabe 1
+# Print numbers from 1 to 10.
+
+
+# Aufgabe 2
+# Print even numbers from 2 to 20.
+
+
+# Aufgabe 3
+names = ["Anna", "Maria", "Liubov", "Olena"]
+
+# Print every name.
+
+
+# Aufgabe 4
+scores = [45, 72, 90, 33, 81, 50]
+
+# Print only scores >= 50.
+
+
+# Aufgabe 5
+count = 1
+
+# Use while to print numbers from 1 to 5.
+
+
+# Aufgabe 6
+# Use for + break.
+# Print numbers from 1 to 10,
+# but stop when i == 6.
+
+
+# Aufgabe 7
+# Use continue.
+# Print numbers from 1 to 5,
+# but skip number 3.
+
+
+# Aufgabe 8
+ages = [12, 17, 18, 25, 16, 30, 14, 21]
+
+# For every age:
+# >= 18 → "Volljährig"
+# < 18 → "Minderjährig"
