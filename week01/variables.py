db4729276dbfdfb224749c31ec07069620d3df3d
Liubov-MW <Lybawa92@gmail.com>
2026-09-10 22:58:06 +0200

    Add variables and print statements in variables.py

---
+++ week01/variables.py
@@ -0,0 +1,15 @@
+# Aufgabe 1: Variablen definieren (Змінні)
+name = "Liubov"      # Name als Text (String)
+alter = 25           # Alter als Zahl (Integer)
+groesse = 1.68       # Größe als Kommazahl (Float)
+ist_student = True   # Status als Wahrheitswert (Boolean)
+
+# Ausgaben auf dem Bildschirm (Вивід на екран)
+print("Name:", name)
+print("Alter:", alter)
+print("Größe:", groesse)
+print("Ist Student:", ist_student)
+
+# Rechnen mit Variablen (Обчислення)
+naechstes_jahr_alter = alter + 1
+print("Nächstes Jahr werde ich:", naechstes_jahr_alter)
